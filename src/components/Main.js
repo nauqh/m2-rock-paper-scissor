bc755c6a78411e721dd3ed9a425415b6a035889c
nauqh <hodominhquan.work@gmail.com>
2026-03-01 12:15:55 +0700

    update(M21): add solution

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,57 +6,57 @@ import Choices from "./Choices";
 import { getRandomGameItem, calculatorUserWinner } from "../utils";
 
 const gameItems = [
-  {
-    url: "/images/paper.png",
-    id: 0,
-    winItemIds: [1],
-    name: "Paper",
-  },
-  {
-    url: "/images/rock.png",
-    id: 1,
-    winItemIds: [2],
-    name: "Rock",
-  },
-  {
-    url: "/images/scissor.png",
-    id: 2,
-    winItemIds: [0],
-    name: "Scissor",
-  },
+	{
+		url: "/images/paper.png",
+		id: 0,
+		winItemIds: [1],
+		name: "Paper",
+	},
+	{
+		url: "/images/rock.png",
+		id: 1,
+		winItemIds: [2],
+		name: "Rock",
+	},
+	{
+		url: "/images/scissor.png",
+		id: 2,
+		winItemIds: [0],
+		name: "Scissor",
+	},
 ];
 
 export default function Main() {
-  const [result, setResult] = useState("N/N");
-  const [userGameItem, setUserGameItem] = useState(null);
-  const [computerGameItem, setComputerGameItem] = useState(null);
+	const [result, setResult] = useState("N/N");
+	const [userGameItem, setUserGameItem] = useState(null);
+	const [computerGameItem, setComputerGameItem] = useState(null);
 
-  const handleGameItemChange = (gameItem) => {
-    setUserGameItem({ ...gameItem });
-  };
+	const handleGameItemChange = (gameItem) => {
+		setUserGameItem({ ...gameItem });
+	};
 
-  useEffect(() => {
-    if (userGameItem) {
-      const computerNewItem = getRandomGameItem(gameItems);
-      setComputerGameItem({ ...computerNewItem });
-      setResult(calculatorUserWinner(userGameItem, computerNewItem));
-    }
-  }, [userGameItem]);
+	useEffect(() => {
+		if (userGameItem) {
+			const computerNewItem = getRandomGameItem(gameItems);
+			setComputerGameItem({ ...computerNewItem });
+			setResult(calculatorUserWinner(userGameItem, computerNewItem));
+		}
+	}, [userGameItem]);
 
-  return (
-    <div className="container">
-      <div className="main">
-        <Result
-          user1GameItem={"Your code here"}
-          user2GameItem={"Your code here"}
-          result={"Your code here"}
-        />
-        <Display />
-        <Choices
-          gameItems={"Your code here"}
-          handleGameItemChange={"Your code here"}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="main">
+				<Result
+					user1GameItem={userGameItem}
+					user2GameItem={computerGameItem}
+					result={result}
+				/>
+				<Display />
+				<Choices
+					gameItems={gameItems}
+					handleGameItemChange={handleGameItemChange}
+				/>
+			</div>
+		</div>
+	);
 }
